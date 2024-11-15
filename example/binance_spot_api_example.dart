7dd937bef7195bd58f0ea60322d591f025e22ef3
hndrbrm <hndrbrm@gmail.com>
2024-11-15 14:50:51 +0700

    Initial Files.

---
+++ example/binance_spot_api_example.dart
@@ -0,0 +1,6 @@
+import 'package:binance_spot_api/binance_spot_api.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/binance_spot_api.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/binance_spot_api_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
