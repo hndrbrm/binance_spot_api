750a3c0ad0f115830b3801bca456a8692d09f0ec
hndrbrm <hndrbrm@gmail.com>
2024-11-27 14:36:39 +0700

    Add docs on order response type enum for testnet.

--- lib/src/enum/order_response_type.dart
+++ lib/src/enum/order_response_type.dart
@@ -2,8 +2,33 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-enum OrderResponseType {
+import '../rest_api/serializer.dart';
+
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#order-response-type-neworderresptype
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#order-response-type-neworderresptype
+enum OrderResponseType implements Serializer {
   ack,
   result,
-  full,
+  full;
+  
+  factory OrderResponseType.deserialize(String string) =>
+    switch (string) {
+      _ack => ack,
+      _result => result,
+      _full => full,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _ack = 'ACK';
+  static const _result = 'RESULT';
+  static const _full = 'FULL';
+
+  @override
+  String serialize() =>
+    switch (this) {
+      ack => _ack,
+      result => _result,
+      full => _full,
+    };
 }
