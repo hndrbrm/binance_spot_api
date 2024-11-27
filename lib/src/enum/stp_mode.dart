0aee4194b2cb5c30954832fb374b987e9f398ebe
hndrbrm <hndrbrm@gmail.com>
2024-11-27 15:53:15 +0700

    Add docs on stp mode enum for testnet.

--- lib/src/enum/stp_mode.dart
+++ lib/src/enum/stp_mode.dart
@@ -2,9 +2,37 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-enum StpMode {
+import '../rest_api/serializer.dart';
+
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#stp-modes
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#stp-modes
+enum StpMode implements Serializer {
   none,
   expireMaker,
   expireTaker,
-  expireBoth,
+  expireBoth;
+
+  factory StpMode.deserialize(String string) =>
+    switch (string) {
+    _none => none,
+    _expireMaker => expireMaker,
+    _expireTaker => expireTaker,
+    _expireBoth => expireBoth,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _none = 'NONE';
+  static const _expireMaker = 'EXPIRE_MAKER';
+  static const _expireTaker = 'EXPIRE_TAKER';
+  static const _expireBoth = 'EXPIRE_BOTH';
+
+  @override
+  String serialize() =>
+    switch (this) {
+      none => _none,
+      expireMaker => _expireMaker,
+      expireTaker => _expireTaker,
+      expireBoth => _expireBoth,
+    };
 }
