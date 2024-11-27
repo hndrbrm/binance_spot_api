0b5b0a4fc9357468e605c38815f4680e7ccc66e1
hndrbrm <hndrbrm@gmail.com>
2024-11-27 15:33:27 +0700

    Add docs on time in force enum for testnet.

--- lib/src/enum/time_in_force.dart
+++ lib/src/enum/time_in_force.dart
@@ -2,8 +2,14 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
+import '../rest_api/serializer.dart';
+
 /// This sets how long an order will be active before expiration.
-enum TimeInForce {
+///
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#time-in-force-timeinforce
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#time-in-force-timeinforce
+enum TimeInForce implements Serializer {
   /// Good Til Canceled
   /// An order will be on the book unless the order is canceled.
   gtc,
@@ -15,5 +21,25 @@ enum TimeInForce {
 
   /// Fill or Kill
   /// An order will expire if the full order cannot be filled upon execution.
-  fok,
+  fok;
+  
+  factory TimeInForce.deserialize(String string) =>
+    switch (string) {
+      _gtc => gtc,
+      _ioc => ioc,
+      _fok => fok,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _gtc = 'GTC';
+  static const _ioc = 'IOC';
+  static const _fok = 'FOK';
+
+  @override
+  String serialize() =>
+    switch (this) {
+      gtc => _gtc,
+      ioc => _ioc,
+      fok => _fok,
+    };
 }
