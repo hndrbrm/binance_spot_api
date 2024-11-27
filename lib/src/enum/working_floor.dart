3b26ac5571abc5a86644d379ee771541eca54782
hndrbrm <hndrbrm@gmail.com>
2024-11-27 14:40:42 +0700

    Add docs on working floor enum for testnet.

--- lib/src/enum/working_floor.dart
+++ lib/src/enum/working_floor.dart
@@ -2,7 +2,29 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
-enum WorkingFloor {
+import '../rest_api/serializer.dart';
+
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#working-floor
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#working-floor
+enum WorkingFloor implements Serializer {
   exchange,
-  sor,
+  sor;
+
+  factory WorkingFloor.deserialize(String string) =>
+    switch (string) {
+      _exchange => exchange,
+      _sor => sor,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _exchange = 'EXCHANGE';
+  static const _sor = 'SOR';
+
+  @override
+  String serialize() =>
+    switch (this) {
+      exchange => _exchange,
+      sor => _sor,
+    };
 }
