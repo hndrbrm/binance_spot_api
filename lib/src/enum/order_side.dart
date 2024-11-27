b8f64d124a968643bc5ba1269908517921dc9781
hndrbrm <hndrbrm@gmail.com>
2024-11-27 11:17:55 +0700

    Add serializer for order side.

--- lib/src/enum/order_side.dart
+++ lib/src/enum/order_side.dart
@@ -2,7 +2,25 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
+/// Reference:
+/// https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#order-side-side
 enum OrderSide {
   buy,
-  sell,
+  sell;
+
+  factory OrderSide.deserialize(String string) =>
+    switch (string) {
+      _buy => buy,
+      _sell => sell,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _buy = 'BUY';
+  static const _sell = 'SELL';
+
+  String serialize() =>
+    switch (this) {
+      buy => _buy,
+      sell => _sell,
+    };
 }
