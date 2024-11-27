f679b1cb90852d761b6523ab53ba4b1097f032ab
hndrbrm <hndrbrm@gmail.com>
2024-11-27 13:43:45 +0700

    Add docs on order status enum for testnet.

--- lib/src/enum/order_status.dart
+++ lib/src/enum/order_status.dart
@@ -2,6 +2,9 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#order-status-status
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#order-status-status
 enum OrderStatus {
   /// The order has been accepted by the engine.
   newOrder,
@@ -34,5 +37,42 @@ enum OrderStatus {
   /// The order was expired by the exchange due to STP. (e.g. an order with
   /// EXPIRE_TAKER will match with existing orders on the book with the same
   /// account or same tradeGroupId)
-  expiredInMatch,
+  expiredInMatch;
+  
+  factory OrderStatus.deserialize(String string) =>
+    switch (string) {
+      _newOrder => newOrder,
+      _pendingNew => pendingNew,
+      _partiallyFilled => partiallyFilled,
+      _filled => filled,
+      _canceled => canceled,
+      _pendingCancel => pendingCancel,
+      _rejected => rejected,
+      _expired => expired,
+      _expiredInMatch => expiredInMatch,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _newOrder = 'NEW';
+  static const _pendingNew = 'PENDING_NEW';
+  static const _partiallyFilled = 'PARTIALLY_FILLED';
+  static const _filled = 'FILLED';
+  static const _canceled = 'CANCELED';
+  static const _pendingCancel = 'PENDING_CANCEL';
+  static const _rejected = 'REJECTED';
+  static const _expired = 'EXPIRED';
+  static const _expiredInMatch = 'EXPIRED_IN_MATCH';
+  
+  String serialized() =>
+    switch (this) {
+      newOrder => _newOrder,
+      pendingNew => _pendingNew,
+      partiallyFilled => _partiallyFilled,
+      filled => _filled,
+      canceled => _canceled,
+      pendingCancel => _pendingCancel,
+      rejected => _rejected,
+      expired => _expired,
+      expiredInMatch => _expiredInMatch,
+    };
 }
