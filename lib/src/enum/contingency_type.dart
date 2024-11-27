f41b430669f013bf5a5f6af1c871238bb66e0ffe
hndrbrm <hndrbrm@gmail.com>
2024-11-27 14:15:33 +0700

    Add docs on contingency type enum for testnet.

--- lib/src/enum/contingency_type.dart
+++ lib/src/enum/contingency_type.dart
@@ -2,7 +2,26 @@
 // All rights reserved. Use of this source code is governed
 // by a BSD-style license that can be found in the LICENSE file.
 
+/// References:
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/enums.md#contingencytype
+/// * https://github.com/binance/binance-spot-api-docs/blob/master/testnet/enums.md#contingencytype
 enum ContingencyType {
   oco,
-  oto,
+  oto;
+
+  factory ContingencyType.deserialize(String string) =>
+    switch (string) {
+      _oco => oco,
+      _oto => oto,
+      _ => throw UnimplementedError(string),
+    };
+
+  static const _oco = 'OCO';
+  static const _oto = 'OTO';
+
+  String serialize() =>
+    switch (this) {
+      oco => _oco,
+      oto => _oto,
+    };
 }
